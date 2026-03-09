3c54d34c7cd8a5db2437a30d89f58616591d8070
Zhiming Zhang <athrun07091988@gmail.com>
2026-03-09 01:41:41 -0400

    Remove unused class

--- model.py
+++ model.py
@@ -1,17 +1,6 @@
 from enum import Enum
 from pydantic import BaseModel
 
-
-class ImageQualityEnum(str, Enum):
-    HIGH = "HIGH"
-    MEDIUM = "MEDIUM"
-    LOW = "LOW"
-
-
-class ImageQuality(BaseModel):
-    quality: ImageQualityEnum
-
-
 class VisualContext(BaseModel):
     summary: str
     possible_causes: list[str]
